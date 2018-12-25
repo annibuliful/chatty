2e2db6dfc65170fcbf36e9d0a5168a8d8bad22be
lagmanzaza <lagmanmc@gmail.com>
2018-12-26 03:21:00 +0700

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,21 +16,20 @@ fastify.register(
 fastify.register(compress, { global: false });
 fastify.register(cors);
 // Declare a route
-fastify.get("/", async (request, reply) => {
-  r.dbCreate("SitePoint")
-    .run()
-    .then(function(response) {
-      console.log(response);
-    })
-    .error(function(err) {
-      console.log("error occured ", err);
-    });
+fastify.get("/setup", async (req, res) => {
+  try {
+    const response = await r.dbCreate("SitePoint").run();
+    res.send(response);
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
 });
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(3000, "0.0.0.0");
     console.log("listening 3000");
   } catch (err) {
     fastify.log.error(err);
