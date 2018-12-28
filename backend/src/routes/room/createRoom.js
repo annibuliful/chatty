dac57e5b896a621fe52db5b5e47e2851e96b32ef
lagmanzaza <lagmanmc@gmail.com>
2018-12-28 18:29:55 +0700

    [Service] create chat room

--- backend/src/routes/room/createRoom.js
+++ backend/src/routes/room/createRoom.js
@@ -1,17 +1,17 @@
 import jwt from 'jsonwebtoken';
 import getUser from '../../db/getData';
 import createRoom from '../../db/createData';
+import getJWT from '../../utils/getJWT';
 
-const checkJWT = async (headers, done) => {
+const checkJWT = async (headers) => {
   try {
-    const token = headers.authorization.replace('Bearer ', '');
-    await jwt.verify(token, process.env.JWT_SECRET);
+    await jwt.verify(getJWT(headers), process.env.JWT_SECRET);
+    return true;
   } catch (e) {
-    done.status(401).send({
-      message: 'no token or invalid token',
-    });
+    return false;
   }
 };
+
 export default {
   method: 'POST',
   url: '/room/create',
@@ -33,6 +33,14 @@ export default {
           },
         },
       },
+      403: {
+        type: 'object',
+        properites: {
+          message: {
+            type: 'string',
+          },
+        },
+      },
       500: {
         type: 'object',
         properites: {
@@ -44,7 +52,29 @@ export default {
     },
   },
   handler: async (req, res) => {
-    await checkJWT(req.headers, res);
-    res.send('sss');
+    if (checkJWT(req.headers)) {
+      const { userId } = jwt.decode(getJWT(req.headers));
+      const userInfo = await getUser('users')
+        .filter({
+          id: userId,
+        })
+        .limit(1);
+      if (userInfo.length !== 0) {
+        const room = await createRoom('rooms', {
+          userId,
+        });
+        res.send({
+          roomId: room.generated_keys,
+        });
+      } else {
+        res.status(403).send({
+          message: 'action forbidden',
+        });
+      }
+    } else {
+      res.status(401).send({
+        message: 'no token or invalid token',
+      });
+    }
   },
 };
