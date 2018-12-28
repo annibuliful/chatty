0a033bb69e0e56ad17d4de7ba09f6848dee27649
lagmanzaza <lagmanmc@gmail.com>
2018-12-28 10:16:43 +0700

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,19 @@
-const fastify = require("fastify")();
-const helmet = require("fastify-helmet");
-const compress = require("fastify-compress");
-const cors = require("fastify-cors");
-const r = require("rethinkdbdash")({
+const fastify = require('fastify')();
+const helmet = require('fastify-helmet');
+const compress = require('fastify-compress');
+const cors = require('fastify-cors');
+const r = require('rethinkdbdash')({
   port: 28015,
-  host: "db"
+  host: 'db',
 });
 // dependencies
-
-fastify.register(
-  helmet,
-  // Example of passing an option to x-powered-by middleware
-  { hidePoweredBy: { setTo: "Chatty" } }
-);
+fastify.register(helmet, { hidePoweredBy: { setTo: 'Chatty' } });
 fastify.register(compress, { global: false });
 fastify.register(cors);
 // Declare a route
-fastify.get("/setup", async (req, res) => {
+fastify.get('/setup', async (req, res) => {
   try {
-    const response = await r.dbCreate("SitePoint").run();
+    const response = await r.dbCreate('chatty').run();
     res.send(response);
   } catch (e) {
     console.log(e);
@@ -29,10 +24,9 @@ fastify.get("/setup", async (req, res) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000, "0.0.0.0");
-    console.log("listening 3000");
+    await fastify.listen(3000, '0.0.0.0');
+    console.log('listening 3000');
   } catch (err) {
-    fastify.log.error(err);
     process.exit(1);
   }
 };
