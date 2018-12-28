dce220afcd455f09467bd3787545fc26190ca7ee
lagmanzaza <lagmanmc@gmail.com>
2018-12-28 19:42:42 +0700

    add validation

--- backend/src/routes/room/createRoom.js
+++ backend/src/routes/room/createRoom.js
@@ -2,12 +2,27 @@ import jwt from 'jsonwebtoken';
 import getUser from '../../db/getData';
 import createRoom from '../../db/createData';
 import getJWT from '../../utils/getJWT';
-import checkJWT from '../../utils/checkJWT';
 
 export default {
   method: 'POST',
   url: '/room/create',
   schema: {
+    body: {
+      type: 'object',
+      properties: {
+        roomName: {
+          type: 'string',
+        },
+      },
+      required: ['roomName'],
+    },
+    headers: {
+      type: 'object',
+      properties: {
+        authorization: { type: 'string' },
+      },
+      required: ['authorization'],
+    },
     response: {
       200: {
         type: 'object',
@@ -44,30 +59,27 @@ export default {
     },
   },
   handler: async (req, res) => {
-    const checkToken = await checkJWT(req.headers);
-    if (checkToken) {
-      const { userId } = jwt.decode(getJWT(req.headers));
-      const userInfo = await getUser('users')
-        .filter({
-          id: userId,
-        })
-        .limit(1);
-      if (userInfo.length !== 0) {
-        try {
-          const room = await createRoom('rooms', {
-            userId,
-          });
-          res.send({
-            roomId: room.generated_keys,
-          });
-        } catch (e) {
-          res.status(500).send({
-            message: e,
-          });
-        }
-      } else {
-        res.status(403).send({
-          message: 'action forbidden',
+    const { userId } = jwt.decode(getJWT(req.headers));
+    const userInfo = await getUser('users')
+      .filter({
+        id: userId,
+      })
+      .limit(1);
+    if (userInfo.length !== 0) {
+      try {
+        const room = await createRoom('rooms', {
+          roomName: req.body.roomName,
+        });
+        await createRoom('member_room', {
+          roomId: room.generated_keys[0],
+          userId,
+        });
+        res.send({
+          roomId: room.generated_keys[0],
+        });
+      } catch (e) {
+        res.status(500).send({
+          message: e,
         });
       }
     } else {
