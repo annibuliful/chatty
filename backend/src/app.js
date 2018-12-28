83b0b9eff00a5e2314984169ab92aa7cfef4fbfc
lagmanzaza <lagmanmc@gmail.com>
2018-12-28 16:40:20 +0700

    [Service] create chat room

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,9 @@ const fastify = require('fastify')();
 const helmet = require('fastify-helmet');
 const compress = require('fastify-compress');
 const cors = require('fastify-cors');
+const httpError = require('fastify-sensible');
 // dependencies
+fastify.register(httpError);
 fastify.register(helmet, { hidePoweredBy: { setTo: 'Chatty' } });
 fastify.register(compress, { global: false });
 fastify.register(cors);
@@ -18,6 +20,7 @@ export default async () => {
     await fastify.listen(3000, '0.0.0.0');
     console.log('listening 3000');
   } catch (err) {
+    console.log(err);
     process.exit(1);
   }
 };
