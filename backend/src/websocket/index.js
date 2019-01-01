96bad10ccdb17613da70ad1956875392a3e4da22
lagmanzaza <lagmanmc@gmail.com>
2019-01-02 01:25:00 +0700

    [Improve] seperate function and action controller into other files

--- backend/src/websocket/index.js
+++ backend/src/websocket/index.js
@@ -1,34 +1,13 @@
 import WebSocket from 'ws';
+import controller from './controllerAction';
 
 export default (server) => {
   const wss = new WebSocket.Server({ server });
-
-  function broadcast(message) {
-    wss.clients.forEach((client) => {
-      console.log(client);
-      if (client.room.indexOf(JSON.parse(message).room) > -1) {
-        client.send(message);
-      }
-    });
-  }
   wss.on('connection', (ws) => {
     ws.room = [];
-    ws.send(JSON.stringify({ msg: 'user joined' }));
-    console.log('connected');
-    ws.on('message', (message) => {
-      console.log('message: ', message);
-      const messag = JSON.parse(message);
-      console.log(messag);
-      if (messag.join) {
-        ws.room.push(messag.join);
-        console.log(ws.room);
-      }
-      if (messag.room) {
-        broadcast(message);
-      }
-      if (messag.msg) {
-        console.log('message: ', messag.msg);
-      }
+    ws.on('message', async (data) => {
+      const message = JSON.parse(data);
+      await controller(message, ws, wss);
     });
 
     ws.on('error', e => console.log(e));
