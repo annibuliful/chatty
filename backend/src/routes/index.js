eb92460f33d8c581743dfeeda2644c9183c991b0
lagmanzaza <lagmanmc@gmail.com>
2019-01-04 16:56:25 +0700

    [Route] add list room route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,5 +3,13 @@ import loginRoute from './login';
 import setupRoute from './setup';
 import createChatRoomRoute from './room/createRoom';
 import joinChatRoomRoute from './room/joinRoom';
+import listRoomsRoute from './room/listRoom';
 
-export default [registerRoute, setupRoute, loginRoute, createChatRoomRoute, joinChatRoomRoute];
+export default [
+  registerRoute,
+  setupRoute,
+  loginRoute,
+  createChatRoomRoute,
+  joinChatRoomRoute,
+  listRoomsRoute,
+];
