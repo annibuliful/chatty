fd59b422f2c3dfb91daa59b618780b665e64c8d6
lagmanzaza <lagmanmc@gmail.com>
2019-01-07 00:50:54 +0700

    [Improvement, Fix] this service don't check user authetication key, so added it

--- backend/src/routes/room/joinRoom.js
+++ backend/src/routes/room/joinRoom.js
@@ -1,7 +1,8 @@
-import jwt from 'jsonwebtoken';
 import getInfo from '../../db/getData';
 import createRoom from '../../db/createData';
 import getJWT from '../../utils/getJWT';
+import decodeJWT from '../../utils/decodeJWT';
+import verifyJWT from '../../utils/verifyJWT';
 
 const checkRoomId = async (roomId) => {
   const roomInfo = await getInfo('rooms').get(roomId);
@@ -40,31 +41,34 @@ export default {
     },
   },
   handler: async (req, res) => {
-    const checkRoom = await checkRoomId(req.params.roomId);
-    const { userId } = jwt.decode(getJWT(req.headers));
-    const userInfo = await getInfo('users')
-      .filter({
-        id: userId,
-      })
-      .limit(1);
-    if (userInfo.length !== 0 && checkRoom) {
-      try {
-        await createRoom('member_room', {
-          roomId: req.params.roomId,
-          userId,
-        });
-        res.send({
-          roomId: req.params.roomId,
-        });
-      } catch (e) {
-        res.status(500).send({
-          message: e,
+    const token = getJWT(req.headers);
+    if (await verifyJWT(token)) {
+      const checkRoom = await checkRoomId(req.params.roomId);
+      const { userId } = decodeJWT(token);
+      const userInfo = await getInfo('users')
+        .filter({
+          id: userId,
+        })
+        .limit(1);
+      if (userInfo.length !== 0 && checkRoom) {
+        try {
+          await createRoom('member_room', {
+            roomId: req.params.roomId,
+            userId,
+          });
+          res.send({
+            roomId: req.params.roomId,
+          });
+        } catch (e) {
+          res.status(500).send({
+            message: e,
+          });
+        }
+      } else {
+        res.status(401).send({
+          message: 'no token or invalid token or not found room id',
         });
       }
-    } else {
-      res.status(401).send({
-        message: 'no token or invalid token or not found room id',
-      });
     }
   },
 };
