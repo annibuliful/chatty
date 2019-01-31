e9e686bb9644cd400c33a994d1ea9a9b0d0da27b
lagmanzaza <lagmanmc@gmail.com>
2019-01-31 13:46:26 +0700

    merge

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,24 +1,27 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-
+import checkToken from "./services/checkToken";
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: "/",
       name: "home",
       component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // }
   ]
 });
+
+router.beforeEach((to, from, next) => {});
+export default router;
